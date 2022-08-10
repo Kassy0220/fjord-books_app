ece88c84d17567c2e0522be372d3f5bb10d69a7f
github Kassy <github@shinbokutaro0220@gmail.com>
2022-08-10 11:02:25 +0900

    Userモデルのname属性に、not null属性を追加しました

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   max_paginates_per 5
+
+  validates :name, presence: true
 end
