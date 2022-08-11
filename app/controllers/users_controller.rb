c8d0440806a38f0a9fbed41beffd2324329d1456
github Kassy <github@shinbokutaro0220@gmail.com>
2022-08-11 10:14:42 +0900

    ユーザーレコードを取得する際レコードを取得する際の並び順を指定しました

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @users = User.page(params[:page])
+    @users = User.order(:id).page(params[:page])
   end
 
   def show
