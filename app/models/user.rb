311b632df025011cbfc0cce378607ea498fbf2a7
github Kassy <github@shinbokutaro0220@gmail.com>
2022-08-13 19:25:34 +0900

    アイコン画像を(jpeg/png/gif)に限定しました

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :icon
+
+  validate :validate_file_type
+
+  def validate_file_type
+    return unless icon.attached?
+
+    valid_file_types = %w(image/jpeg image/png image/gif)
+    if !valid_file_types.include?(icon.blob.content_type)
+      errors.add(:icon, I18n.t('errors.messages.invalid_file_type'))
+    end
+  end
 end
