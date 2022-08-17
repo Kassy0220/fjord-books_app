deec005776e785e6bd0f50114dc35907a8574c93
github Kassy <github@shinbokutaro0220@gmail.com>
2022-08-17 20:22:51 +0900

    複数に分かれていたマイグレーションファイルを1つにまとめました

--- db/migrate/20220817111117_add_columns_to_users.rb
+++ db/migrate/20220817111117_add_columns_to_users.rb
@@ -1,5 +1,6 @@
 class AddColumnsToUsers < ActiveRecord::Migration[6.1]
   def change
+    add_column :users, :name, :string, null: false
     add_column :users, :post_code, :string
     add_column :users, :address, :string
     add_column :users, :biography, :text
