dc2c7e66405abb52373fb00bb16b1955a4d835e0
github Kassy <github@shinbokutaro0220@gmail.com>
2022-08-20 18:16:35 +0900

    ユーザー詳細ページで、フォロー数/フォロワー数を表示する様にしました

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,6 @@ class User < ApplicationRecord
                               dependent: :destroy, inverse_of: :follower 
   has_many :passive_followings, class_name: 'Following', foreign_key: 'followed_id',
                               dependent: :destroy, inverse_of: :followed
+  has_many :followings, through: :active_followings, source: :followed
+  has_many :followers, through: :passive_followings, source: :follower
 end
