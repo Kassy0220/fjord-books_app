c6dfd011a6d443fcf25495c33cc5371fe3fa35f8
github Kassy <github@shinbokutaro0220@gmail.com>
2022-09-01 07:46:20 +0900

    ユーザーの名前もしくはメールアドレスを表示する処理を、Userモデルのインスタンスメソッドにしました

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,4 +31,8 @@ class User < ApplicationRecord
     relationship = active_relationships.find_by(following_id: user.id)
     relationship&.destroy!
   end
+
+  def name_or_email
+    attribute_present?(:name) ? self.name : self.email
+  end
 end
