8f18801d42b4fd299219d63ae89ee97030051d7c
github Kassy <github@shinbokutaro0220@gmail.com>
2022-09-10 18:12:06 +0900

    Report#editable? に対するテストを作成しました

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,16 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#editable? should return true when user is author" do
+    alpha = User.create!(email: 'alpha@example.com', password: 'password')
+    report = alpha.reports.create!(title: 'Hello', content: 'This is a first report!')
+    assert report.editable?(alpha)
+  end
+
+  test "#editable? should return false when user is not author" do
+    alpha = User.create!(email: 'alpha@example.com', password: 'password')
+    bravo = User.create!(email: 'bravo@example.com', password: 'password')
+    report = alpha.reports.create!(title: 'Hello', content: 'This is a first report!')
+    assert_not report.editable?(bravo)
+  end
 end
