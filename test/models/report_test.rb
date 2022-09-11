673d4e7b89a6aefc7ec42a82c9e488d674891912
github Kassy <github@shinbokutaro0220@gmail.com>
2022-09-12 08:45:28 +0900

    Reportモデルのテストの重複箇所を修正しました

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,22 +3,23 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
+  def setup
+    @alpha = users(:alpha)
+    @bravo = users(:bravo)
+  end
+
   test "#editable? should return true when user is author" do
-    alpha = User.create!(email: 'alpha@example.com', password: 'password')
-    report = alpha.reports.create!(title: 'Hello', content: 'This is a first report!')
-    assert report.editable?(alpha)
+    report = @alpha.reports.create!(title: 'Hello', content: 'This is a first report!')
+    assert report.editable?(@alpha)
   end
 
   test "#editable? should return false when user is not author" do
-    alpha = User.create!(email: 'alpha@example.com', password: 'password')
-    bravo = User.create!(email: 'bravo@example.com', password: 'password')
-    report = alpha.reports.create!(title: 'Hello', content: 'This is a first report!')
-    assert_not report.editable?(bravo)
+    report = @alpha.reports.create!(title: 'Hello', content: 'This is a first report!')
+    assert_not report.editable?(@bravo)
   end
   
   test "#created_on should return the day of the week and date when report was created" do
-    alpha = User.create!(email: 'alpha@example.com', password: 'password')
-    report = alpha.reports.create!(title: 'Hello', content: 'This is a first report!', created_at: Time.zone.parse('2021-12-31 12:00:00'))
+    report = @alpha.reports.create!(title: 'Hello', content: 'This is a first report!', created_at: Time.zone.parse('2021-12-31 12:00:00'))
     assert_equal Date.new(2021, 12, 31), report.created_on
   end
 end
