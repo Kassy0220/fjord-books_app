815b5d4d2a07126f2188d43bdad321943dd12ab3
github Kassy <github@shinbokutaro0220@gmail.com>
2022-09-12 08:38:49 +0900

    fixtureにサンプルユーザーを追加し、Userモデルのテストの重複箇所を修正しました

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,6 +3,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  def setup
+    @alpha = users(:alpha)
+    @bravo = users(:bravo)
+  end
+
   test "#name_or_email should return email when unnamed user" do
     user = User.new(email: 'foo@example.com', name: '')
     assert_equal 'foo@example.com', user.name_or_email
@@ -14,37 +19,29 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "#follow should enable user to follow other user" do
-    alpha = User.create!(email: 'alpha@example.com', password: 'password')
-    bravo = User.create!(email: 'bravo@example.com', password: 'password')
-    assert_difference 'alpha.followings.size', 1 do
-      alpha.follow bravo
-      alpha.followings.reload
+    assert_difference '@alpha.followings.size', 1 do
+      @alpha.follow @bravo
+      @alpha.followings.reload
     end
   end
 
   test "#unfollow should enable user to unfollow other user" do
-    alpha = User.create!(email: 'alpha@example.com', password: 'password')
-    bravo = User.create!(email: 'bravo@example.com', password: 'password')
-    alpha.follow bravo
-    assert_difference 'alpha.followings.size', -1 do
-      alpha.unfollow bravo
-      alpha.followings.reload
+    @alpha.follow @bravo
+    assert_difference '@alpha.followings.size', -1 do
+      @alpha.unfollow @bravo
+      @alpha.followings.reload
     end
   end
   
   test "#following? should return true when user is following other user" do
-    alpha = User.create!(email: 'alpha@example.com', password: 'password')
-    bravo = User.create!(email: 'bravo@example.com', password: 'password')
-    assert_not alpha.following?(bravo)
-    alpha.follow bravo
-    assert alpha.following?(bravo)
+    assert_not @alpha.following?(@bravo)
+    @alpha.follow @bravo
+    assert @alpha.following?(@bravo)
   end
 
   test "#followed_by? should return true when user is followed by other user" do
-    alpha = User.create!(email: 'alpha@example.com', password: 'password')
-    bravo = User.create!(email: 'bravo@example.com', password: 'password')
-    assert_not alpha.followed_by?(bravo)
-    bravo.follow alpha
-    assert alpha.followed_by?(bravo)
+    assert_not @alpha.followed_by?(@bravo)
+    @bravo.follow @alpha
+    assert @alpha.followed_by?(@bravo)
   end
 end
