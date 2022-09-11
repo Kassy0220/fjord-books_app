d224331248b166ceaed2b9bac97c0486e2b62ff2
github Kassy <github@shinbokutaro0220@gmail.com>
2022-09-12 08:16:37 +0900

    Report#created_on に対するテストを作成しました

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -15,4 +15,10 @@ class ReportTest < ActiveSupport::TestCase
     report = alpha.reports.create!(title: 'Hello', content: 'This is a first report!')
     assert_not report.editable?(bravo)
   end
+  
+  test "#created_on should return the day of the week and date when report was created" do
+    alpha = User.create!(email: 'alpha@example.com', password: 'password')
+    report = alpha.reports.create!(title: 'Hello', content: 'This is a first report!', created_at: Time.zone.parse('2021-12-31 12:00:00'))
+    assert_equal Date.new(2021, 12, 31), report.created_on
+  end
 end
