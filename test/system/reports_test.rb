c29c0418d3f40405c1ab49ed7d8506be7a6c0d94
github Kassy <github@shinbokutaro0220@gmail.com>
2022-09-12 18:43:44 +0900

    日報更新のシステムテストを作成しました

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -26,5 +26,21 @@ class ReportsTest < ApplicationSystemTestCase
     assert_text '操作をシミュレートするシステムテストは面白いです！'
     assert_text 'Alpha'
   end
+
+  test 'User can edit report' do
+    click_link '日報'
+    click_link '編集'
+    assert_selector 'h1', text: '日報の編集'
+    assert_field 'タイトル', with: '最初の日報'
+    assert_field '内容', with: '日報初投稿です！！！'
+
+    fill_in 'タイトル', with: '二日目の日報'
+    fill_in '内容', with: '二日目の日報です'
+    click_button '更新する'
+    assert_selector 'h1', text: '日報の詳細'
+    assert_text '日報が更新されました'
+    assert_text '二日目の日報'
+    assert_text '二日目の日報です'
+  end
 end
 
