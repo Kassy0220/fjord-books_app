af6492d7cc100c037ccd17b5d853371fd1477981
github Kassy <github@shinbokutaro0220@gmail.com>
2022-09-12 19:06:55 +0900

    Reportモデルのrubocop指摘箇所を修正しました

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -8,17 +8,17 @@ class ReportTest < ActiveSupport::TestCase
     @bravo = users(:bravo)
   end
 
-  test "#editable? should return true when user is author" do
+  test '#editable? should return true when user is author' do
     report = @alpha.reports.create!(title: 'Hello', content: 'This is a first report!')
     assert report.editable?(@alpha)
   end
 
-  test "#editable? should return false when user is not author" do
+  test '#editable? should return false when user is not author' do
     report = @alpha.reports.create!(title: 'Hello', content: 'This is a first report!')
     assert_not report.editable?(@bravo)
   end
-  
-  test "#created_on should return the day of the week and date when report was created" do
+
+  test '#created_on should return the day of the week and date when report was created' do
     report = @alpha.reports.create!(title: 'Hello', content: 'This is a first report!', created_at: Time.zone.parse('2021-12-31 12:00:00'))
     assert_equal Date.new(2021, 12, 31), report.created_on
   end
