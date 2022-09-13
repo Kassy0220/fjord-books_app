6390861969ea6164a5148e91fe06471b64f3eb84
github Kassy <github@shinbokutaro0220@gmail.com>
2022-09-13 10:49:46 +0900

    本を更新するシステムテストを作成しました

--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -5,6 +5,7 @@ require 'application_system_test_case'
 class BooksTest < ApplicationSystemTestCase
   setup do
     sign_in_as users(:alpha)
+    @book = books(:cherry_book)
   end
 
   test 'creating a Book' do
@@ -20,5 +21,22 @@ class BooksTest < ApplicationSystemTestCase
     assert_text 'Ruby超入門'
     assert_text '初めての方でもわかりやすい内容です！'
     assert_text '五十嵐邦明'
+  end
+
+  test 'updating a Book' do
+    visit books_url
+    click_link '編集', href: edit_book_path(@book)
+    assert_field 'タイトル', with: 'プロを目指す人のためのRuby入門'
+    assert_field 'メモ', with: '深い内容まで丁寧に解説してある名著です！'
+    assert_field '著者', with: '伊藤淳一'
+
+    fill_in 'タイトル', with: 'ブラックジャック'
+    fill_in 'メモ', with: '無免許の天才外科医ブラック・ジャックが活躍する医学ドラマです。'
+    fill_in '著者', with: '手塚治虫'
+    click_on '更新する'
+    assert_text '本が更新されました。'
+    assert_text 'ブラックジャック'
+    assert_text '無免許の天才外科医ブラック・ジャックが活躍する医学ドラマです。'
+    assert_text '手塚治虫'
   end
 end
