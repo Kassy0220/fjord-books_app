824d537099137b02bbc6b6c7e8ba7cd40556c5e6
github Kassy <github@shinbokutaro0220@gmail.com>
2022-09-13 10:19:03 +0900

    本を新規作成するシステムテストを作成しました

--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -4,44 +4,21 @@ require 'application_system_test_case'
 
 class BooksTest < ApplicationSystemTestCase
   setup do
-    @book = books(:one)
-  end
-
-  test 'visiting the index' do
-    visit books_url
-    assert_selector 'h1', text: 'Books'
+    sign_in_as users(:alpha)
   end
 
   test 'creating a Book' do
     visit books_url
-    click_on 'New Book'
+    click_on '新規作成'
 
-    fill_in 'Memo', with: @book.memo
-    fill_in 'Title', with: @book.title
-    click_on 'Create Book'
+    fill_in 'タイトル', with: 'Ruby超入門'
+    fill_in 'メモ', with: '初めての方でもわかりやすい内容です！'
+    fill_in '著者', with: '五十嵐邦明'
+    click_on '登録する'
 
-    assert_text 'Book was successfully created'
-    click_on 'Back'
-  end
-
-  test 'updating a Book' do
-    visit books_url
-    click_on 'Edit', match: :first
-
-    fill_in 'Memo', with: @book.memo
-    fill_in 'Title', with: @book.title
-    click_on 'Update Book'
-
-    assert_text 'Book was successfully updated'
-    click_on 'Back'
-  end
-
-  test 'destroying a Book' do
-    visit books_url
-    page.accept_confirm do
-      click_on 'Destroy', match: :first
-    end
-
-    assert_text 'Book was successfully destroyed'
+    assert_text '本が作成されました'
+    assert_text 'Ruby超入門'
+    assert_text '初めての方でもわかりやすい内容です！'
+    assert_text '五十嵐邦明'
   end
 end
