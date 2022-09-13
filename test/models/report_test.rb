d1fb114fcdb01a5bf3e5f72938b524fc8f9b49db
github Kassy <github@shinbokutaro0220@gmail.com>
2022-09-14 08:53:36 +0900

    Reportモデルのテストを、FactoryBotを利用するように変更しました

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -4,22 +4,21 @@ require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
   def setup
-    @alpha = users(:alpha)
-    @bravo = users(:bravo)
+    @user = create(:user)
+    @report = create(:report, user: @user)
   end
 
   test '#editable? should return true when user is author' do
-    report = @alpha.reports.create!(title: 'Hello', content: 'This is a first report!')
-    assert report.editable?(@alpha)
+    assert @report.editable?(@user)
   end
 
   test '#editable? should return false when user is not author' do
-    report = @alpha.reports.create!(title: 'Hello', content: 'This is a first report!')
-    assert_not report.editable?(@bravo)
+    another_user = create(:user, email: 'bar@example.com')
+    assert_not @report.editable?(another_user)
   end
 
   test '#created_on should return the day of the week and date when report was created' do
-    report = @alpha.reports.create!(title: 'Hello', content: 'This is a first report!', created_at: Time.zone.parse('2021-12-31 12:00:00'))
+    report = create(:report, user: @user, created_at: Time.zone.parse('2021-12-31 12:00:00'))
     assert_equal Date.new(2021, 12, 31), report.created_on
   end
 end
