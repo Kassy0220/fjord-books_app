298c80b7e9464dfd22fab77987e502dab25dfb10
github Kassy <github@shinbokutaro0220@gmail.com>
2022-09-14 11:34:50 +0900

    RelationshipのシステムテストをFactoryBotを利用するように変更しました

--- test/system/relationships_test.rb
+++ test/system/relationships_test.rb
@@ -4,15 +4,17 @@ require 'application_system_test_case'
 
 class RelationshipsTest < ApplicationSystemTestCase
   def setup
-    sign_in_as users(:alpha)
+    user = create(:user)
+    sign_in_as user
+    @another_user = create(:user, email: 'bar@example.com')
   end
 
   test 'User can follow and unfollow other user' do
     click_link 'ユーザ'
     assert_selector 'h1', text: 'ユーザ'
 
-    all('tbody tr')[1].click_link '詳細'
-    assert_text 'bravo@example.com'
+    click_link '詳細', href: user_path(@another_user)
+    assert_text 'bar@example.com'
     assert_text '0 フォロワー'
     assert_button 'フォローする'
 
