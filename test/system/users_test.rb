02fd421369bd4ad06531db5f7254cbfec5a90aea
github Kassy <github@shinbokutaro0220@gmail.com>
2022-09-14 09:45:31 +0900

    UserのシステムテストをFactoryBotを利用するように変更しました

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -4,19 +4,19 @@ require 'application_system_test_case'
 
 class UsersTest < ApplicationSystemTestCase
   def setup
-    @alpha = users(:alpha)
-    sign_in_as @alpha
+    @user = create(:user)
+    sign_in_as @user
   end
 
   test 'User can add avatar image to profile' do
     click_link 'アカウント編集'
     assert_selector 'h2', text: 'アカウント編集'
-    assert_not @alpha.avatar.attached?
+    assert_not @user.avatar.attached?
 
-    attach_file 'ユーザ画像', Rails.root.join('test/fixtures/files/avatar-1.png')
+    attach_file 'ユーザ画像', Rails.root.join('test/factories/files/avatar-1.png')
     fill_in '現在のパスワード', with: 'password'
     click_button '更新'
-    assert @alpha.reload.avatar.attached?
+    assert @user.reload.avatar.attached?
     assert_selector "img[src$='avatar-1.png']"
   end
 end
